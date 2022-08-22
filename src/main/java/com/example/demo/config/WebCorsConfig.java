351b0edee5c58986c5217083ab520157ea610f27
TSRI\Jeff <jeff.hsu@frog-jump.com>
2022-08-22 09:01:54 +0800

    cors global

---
+++ src/main/java/com/example/demo/config/WebCorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static com.example.demo.config.Constant.FRONT_END_URL;
+
+@EnableWebMvc
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/test/**")
+                .allowedOrigins(FRONT_END_URL)
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -4,16 +4,11 @@ import com.example.demo.model.BaseUser;
 import com.example.demo.repo.BaseUserRepo;
 import com.example.demo.repo.ProUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import static com.example.demo.config.Constant.FRONT_END_URL;
-
-@CrossOrigin(value = FRONT_END_URL)
 @RestController
 @RequestMapping("/test")
 public class controller {
